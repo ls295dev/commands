c38816723f23d546752bb84763cae02394d805a9
Luca295 <161210679+ls295dev@users.noreply.github.com>
2026-06-26 17:51:44 +0200

    Refactor chezmoi-push.sh to use chezmoi git commands

--- chezmoi-push.sh
+++ chezmoi-push.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 chezmoi re-add
-cd "~/.local/share/chezmoi"
-git add .
-git commit -m "$(date +%Y%m%d%H%M%S)"
-git push
-cd
+chezmoi git add .
+chezmoi git commit -m "$(date +%Y%m%d%H%M%S)"
+chezmoi git push
