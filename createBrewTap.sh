53932086f4366d9887448b822e8a19b9b26b9af6
Luca295 <161210679+ls295dev@users.noreply.github.com>
2026-06-27 14:40:33 +0200

    Remove brew install command from createBrewTap.sh

--- createBrewTap.sh
+++ createBrewTap.sh
@@ -1,4 +1,3 @@
 YOUR_GITHUB_USERNAME="lsdev295"
 brew tap-new $YOUR_GITHUB_USERNAME/homebrew-tap
-brew install gh
 gh repo create $YOUR_GITHUB_USERNAME/homebrew-tap --push --public --source "$(brew --repository $YOUR_GITHUB_USERNAME/homebrew-tap)"
