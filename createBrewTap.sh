2aad64a1b39cb58ba177097fee1e823edcb17f90
Luca295 <161210679+ls295dev@users.noreply.github.com>
2026-06-27 15:33:28 +0200

    Update GitHub username in createBrewTap.sh

--- createBrewTap.sh
+++ createBrewTap.sh
@@ -1,3 +1,3 @@
-YOUR_GITHUB_USERNAME="lsdev295"
+YOUR_GITHUB_USERNAME="ls295dev"
 brew tap-new $YOUR_GITHUB_USERNAME/homebrew-tap
 gh repo create $YOUR_GITHUB_USERNAME/homebrew-tap --push --public --source "$(brew --repository $YOUR_GITHUB_USERNAME/homebrew-tap)"
